3c052e7008cc998d0ff26a4fd6ac7f1ed83f63eb
zhangdd <2067949563@qq.com>
2024-08-13 08:18:01 +0800

    m

--- p8_file_syscall/my_cat.c
+++ p8_file_syscall/my_cat.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[]) {
     int fd, n;
-    char buf[1028];
+    char buf[1024];
 
     fd = 0;
     if (argc > 1) {
@@ -15,7 +15,21 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    while ((n = read(fd, buf, sizeof(buf)))) {
-        write(1, buf, n);
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        if (write(1, buf, n)) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
     }
+
+    if (n < 0) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fd != 0) {
+        close(fd);
+    }
+
+    return 0;
 }
